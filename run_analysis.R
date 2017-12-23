78e3328ea0f9e85e5fb03dccc923d2991e306eba
swapnaj1234 <swapnaj@hotmail.com>
2017-12-23 14:05:29 -0700

    tidy data project

--- run_analysis.R
+++ run_analysis.R
@@ -2,7 +2,7 @@ library(data.table)
 library(dplyr)
 
 
-featureNames <- read.table("~/UCI HAR Dataset/features.txt")
+featureLabels <- read.table("~/UCI HAR Dataset/features.txt")
 activityLabels <- read.table("~/UCI HAR Dataset/activity_labels.txt", header = FALSE)
 
 subject_training_data <- read.table("~/UCI HAR Dataset/train/subject_train.txt", header = FALSE)
@@ -13,33 +13,24 @@ subject_training_test <- read.table("~/UCI HAR Dataset/test/subject_test.txt", h
 activity_training_test <- read.table("~/UCI HAR Dataset/test/y_test.txt", header = FALSE)
 features_training_test <- read.table("~/UCI HAR Dataset/test/X_test.txt", header = FALSE)
 
-subject <- rbind(subject_training_data, subject_training_test)
-activity <- rbind(activity_training_data, activity_training_test)
-features <- rbind(features_training_data, features_training_test)
-
-colnames(features) <- t(featureNames[2])
-
-colnames(activity) <- "Activity"
-colnames(subject) <- "Subject"
-mergeddata <- cbind(features,activity,subject)
-columnsWithMeanSTD <- grep(".*Mean.*|.*Std.*", names(mergeddata), ignore.case=TRUE)
-requiredColumns <- c(columnsWithMeanSTD, 562, 563)
-dim(mergeddata)
-
-resultData <- mergeddata[,requiredColumns]
+mergeddata <- rbind(
+  cbind(subject_training_data, features_training_data, activity_training_data),
+  cbind(subject_training_test, features_training_test, activity_training_test)
+)
+colnames(mergeddata) <- c("subject", t(featureLabels[2]), "activity")
+columns_result <- grep("subject|activity|.*Mean.*|.*Std.*", colnames(mergeddata), ignore.case=TRUE)
+resultData <- mergeddata[,columns_result]
 dim(resultData)
 
-resultData$Activity <- as.character(resultData$Activity)
+resultData$activity <- as.character(resultData$activity)
 for (i in 1:6){
-  resultData$Activity[resultData$Activity == i] <- as.character(activityLabels[i,2])
+  resultData$activity[resultData$activity == i] <- as.character(activityLabels[i,2])
 }
 
-
-resultData$Activity <- as.factor(resultData$Activity)
-
-resultData$Subject <- as.factor(resultData$Subject)
+resultData$activity <- as.factor(resultData$activity)
+resultData$subject <- as.factor(resultData$subject)
 resultData <- data.table(resultData)
 
-tidyData <- aggregate(. ~Subject + Activity, resultData, mean)
-tidyData <- tidyData[order(tidyData$Subject,tidyData$Activity),]
+tidyData <- aggregate(. ~subject + activity, resultData, mean)
+tidyData <- tidyData[order(tidyData$subject,tidyData$activity),]
 write.table(tidyData, file = "~/Tidy.txt", row.names = FALSE)
